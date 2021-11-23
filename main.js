5270f0caba493465b09cb897fd7dffb715b0ce7a
Tanya Damgaard <damgaardtanya@gmail.com>
2021-11-23 13:58:04 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,53 @@
+//FRÅGOR
+
+// const AllQuestions = [
+//     {
+//         question: 'Det finns inga pingviner i Afrika.',
+//         a: "Sant",
+//         b: "Falskt", //RÄTT
+//         correct: "b",
+//     },
+//     {
+//         question: 'Trots storleken på deras hjärnor har elefanter dåligt minne.',
+//         a: "Sant",
+//         b: "Falskt", //RÄTT
+//         correct: "b",
+//     },
+//     {
+//         question: 'Hästar kan sova ståendes.',
+//         a: "Sant", //RÄTT
+//         b: "Falskt",
+//         correct: "a",
+//     },
+//     {
+//         question: 'Vilda elefanter finns i två världsdelar, vilka? ',
+//         a: "Afrika, Asien", //RÄTT
+//         b: "Sydamerika, Asien",
+//         c: "Europa, Afrika",
+//         correct: "a",
+//     },
+//     {
+//         question: 'Vilken haj är världens största fiskart?',
+//         a: "Vithaj",
+//         b: "Valhaj", //RÄTT
+//         c: "Pigghaj",
+//         correct: "b",
+//     },
+//     {
+//         question: 'Vad kallas sälungen med ett ord på tre bokstäver?',
+//         a: "Pul",
+//         b: "Sut",
+//         c: "Kut", //RÄTT
+//         correct: "c",
+//     },
+//     {
+//         question: 'Vilka svanarter förekommer i Sverige?',
+//         a: "Sångsvan", //RÄTT
+//         b: "Svarthalsad svan",
+//         c: "Knölsvan", //RÄTT
+//         d: "Svart svan",
+
+//         correct: "a",
+//         correct: "c",
+//     }
+// ];
